e95eda159e458e92258ed901f07e345660a5b3ba
mrozinsky <mrozinsky@gmail.com>
2021-09-18 22:00:41 +0200

    second

--- sqrt.c
+++ sqrt.c
@@ -4,20 +4,14 @@
 #include <stdint.h> // needed for uint32_t
 #include <math.h>
 
+
 int main()
 {
-	//uint32_t a = 10000;
-	//uint32_t b = 10000;
-	//for (uint32_t i =0;i <10001; i++)
-	//{
-	//if (a == i)
-	//{
-//		printf("takie same");
-	//}
-//}
+
 	clock_t start = clock();
 	
 	float result = 0;
+	
 	uint32_t test = 1000;
 	//                   4294967295
 	//                   0987654321
@@ -25,14 +19,20 @@ int main()
 	{
 		//if (i%1000000000==0) printf("%u\n",i);
 		//result = sqrt(i);
-		if (i == test) printf("hello");
+		if (i == test) printf("hello\n");
 	}
 	
-	for (uint32_t i=0; i<1000000000; i++)
+		for (uint32_t i=0; i<1000000000; i++)
 	{
 		//if (i%1000000000==0) printf("%u\n",i);
 		//result = sqrt(i);
-		if (i == test) printf("hello");
+		if (i == test) printf("hello\n");
+	}
+		for (uint32_t i=0; i<1000000000; i++)
+	{
+		//if (i%1000000000==0) printf("%u\n",i);
+		//result = sqrt(i);
+		if (i == test) printf("hello\n");
 	}
 	
 	//for (uint32_t i=0; i<1000000000; i++)
@@ -55,11 +55,13 @@ int main()
 	clock_t end = clock();
 	float execution_time = (float)(end - start) / CLOCKS_PER_SEC;
 	
-	printf("\e[1;1H\e[2J");
-	printf("\033[38:2:255:0:255m******************");
+	//printf("\e[1;1H\e[2J");
+	//printf("\033[38:2:255:0:255m******************");
+	printf("\033[96m******************");
 	printf("Program that calculates square roots of numbers from 0 to 4294967295");
 	printf("Execution time: \n%f\n", execution_time);
 	printf("******************");
 
 	return 0;
+	
 }
